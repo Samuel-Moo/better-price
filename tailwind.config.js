1bf0ceee2e11b5c0f8b53bd85852b5df99725166
Omar Contreras <108770789+0marContreras@users.noreply.github.com>
2023-03-29 00:46:45 -0600

    Maquetacion de front y animacion de burbujas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+  theme:{
+    extend:{
+      width:{
+        '128':'32rem'
+      }
+    }
   },
-  plugins: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          
+          "primary": "#FFFFFF",
+                   
+          "secondary": "#152747",
+                   
+          "accent": "#513448",
+                   
+          "neutral": "#171618",
+                   
+          "base-100": "#09090B",
+                   
+          "info": "#66C6FF",
+                   
+          "success": "#87D039",
+                   
+          "warning": "#E2D562",
+                   
+          "error": "#FF6F6F",
+                   },
+      },
+    ],
+  },
+  plugins: [
+     require("daisyui")
+  ],
 }
 
