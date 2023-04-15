9632362ff477ae858b6ba85d5c4abae4c9e232a7
Omar Contreras <108770789+0marContreras@users.noreply.github.com>
2023-04-15 11:26:38 -0600

    amazon y mercadolibre

--- src/pages/api/amazonController.js
+++ src/pages/api/amazonController.js
@@ -1,22 +1,33 @@
+// Este es un controlador de ruta para manejar solicitudes HTTP POST en Next.js
 export default async (req, res) => {
-    if (req.method === "POST") {
-      const { input } = req.body;
-      const responses = await Amazon(input);
-      res.json({ result: responses }); // la clave en el objeto enviado debe ser "result"
-    } else {
-      res.status(404).send("Not found");
-    }
-  };
-  
-  async function Amazon(product) {
-    const amazonScraper = require("amazon-buddy");
-  
-    try {
-      const products = await amazonScraper.products({ keyword: product });
-      console.log(products.result[1].price.current_price);
-      return [products.result[3]]; // devolvemos un array en lugar de un objeto
-    } catch (error) {
-      console.log(error);
-    }
+  // Verifica si la solicitud es una solicitud POST
+  if (req.method === "POST") {
+    // Extrae el valor de "input" del cuerpo de la solicitud
+    const { input } = req.body;
+    // Llama a la función "Amazon" con el valor de entrada y espera la respuesta
+    const responses = await Amazon(input);
+    // Envía la respuesta como un objeto JSON en la respuesta HTTP
+    res.json({ result: responses }); // la clave en el objeto enviado debe ser "result"
+  } else {
+    // Si la solicitud no es una solicitud POST, devuelve un error 404
+    res.status(404).send("Not found");
   }
+};
+
+// Esta es la función "Amazon" que se llama en el controlador de ruta anterior
+async function Amazon(product) {
+  // Importa la biblioteca "amazon-buddy"
+  const amazonScraper = require("amazon-buddy");
   
+  try {
+    // Realiza una búsqueda de productos en Amazon utilizando el valor de entrada
+    const products = await amazonScraper.products({ keyword: product });
+    // Imprime el precio del segundo producto en la respuesta
+    console.log(products.result[1].price.current_price);
+    // Devuelve un arreglo que contiene la información del cuarto producto en la respuesta
+    return [products.result[3]]; // devolvemos un array en lugar de un objeto
+  } catch (error) {
+    // Si hay un error en la búsqueda, imprime el error en la consola
+    console.log(error);
+  }
+}
--- src/pages/api/mlController.js
+++ src/pages/api/mlController.js
@@ -1,23 +1,36 @@
+// Este es un controlador de ruta para manejar solicitudes HTTP POST en Next.js
 export default async (req, res) => {
+  // Verifica si la solicitud es una solicitud POST
   if (req.method === "POST") {
+    // Extrae el valor de "input" del cuerpo de la solicitud
     const { input } = req.body;
+    // Llama a la función "Meli" con el valor de entrada y espera la respuesta
     const responses = await Meli(input);
+    // Envía la respuesta como un objeto JSON en la respuesta HTTP
     res.json({ result: responses }); // la clave en el objeto enviado debe ser "result"
   } else {
+    // Si la solicitud no es una solicitud POST, devuelve un error 404
     res.status(404).send("Not found");
   }
 };
 
+// Esta es la función "Meli" que se llama en el controlador de ruta anterior
 async function Meli(product) {
+  // Importa la biblioteca "mercadolibre"
   var meli = require("mercadolibre");
+  // Crea un objeto "meliObject" utilizando las credenciales de la API de MercadoLibre
   var meliObject = new meli.Meli(process.env.MLID, process.env.MLSECRET);
 
+  // Devuelve una nueva promesa que hace una solicitud a la API de MercadoLibre
   return new Promise((resolve, reject) => {
+    // Hace una solicitud GET a la API de MercadoLibre para buscar productos que coincidan con el valor de entrada
     meliObject.get(`sites/MLM/search?q=${product}`, function (err, res) {
       if (err) {
+        // Si hay un error en la solicitud, rechaza la promesa con el error
         console.log(err);
         reject(err);
       } else {
+        // Si la solicitud tiene éxito, extrae la información del primer producto en la respuesta
         const response = {
           id: res.results[0].id,
           title: res.results[0].title,
@@ -25,7 +38,9 @@ async function Meli(product) {
           image: res.results[0].thumbnail,
           url: res.results[0].permalink,
         };
+        // Imprime la información del producto en la consola
         console.log(response);
+        // Resuelve la promesa con un arreglo que contiene la información del producto
         resolve([response]);
       }
     });
